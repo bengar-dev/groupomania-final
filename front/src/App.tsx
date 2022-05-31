e30706e8881ddd64f9b60ba7a41009ce7701c52a
benysserp <benoitgar.pro@gmail.com>
2022-05-31 10:32:33 +0200

    add: tailwind, react-router, 404 pages, firstcomponent

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom'
+
+import Signin from './pages/signin';
+import Error from './pages/404'
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="bg-zinc-100 min-h-screen">
+      <Routes>
+        <Route path='/' element={<Signin />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
     </div>
   );
 }
