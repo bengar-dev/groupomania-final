75e96dea7e680b01ea679963cb93f81de5219ff7
benysserp <benoitgar.pro@gmail.com>
2022-05-31 23:33:05 +0200

    update: signin page, add: signup page

--- front/src/pages/Signup.tsx
+++ front/src/pages/Signup.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import {Link} from 'react-router-dom'
 
 //components
 import ButtonForm from "../components/ButtonForm";
 //images
 import GroupoLogoWhite from "../assets/icon-left-font-monochrome-white.svg";
 
-export default function Signin() {
+export default function Signup() {
 
   return (
     <div className="min-h-screen flex items-center justify-center">
@@ -15,24 +14,38 @@ export default function Signin() {
         className="h-10"
         src={GroupoLogoWhite} alt="Logo Groupomania" />
         <div className="mt-4 w-4/12 h-px bg-rose-500"></div>
-        <p className="mt-2 text-xs text-white text-center"><span className="text-rose-500 font-medium">Groupomania</span> is the most famous and easiest social network for enterprise. Try it <Link to='/register'><span className="text-rose-500 hover:text-rose-300 font-medium">now</span></Link></p>
+        <p className="mt-2 text-xs text-white text-center">Start now and share your thought with your workmate !</p>
         <form className="mt-4 w-full flex flex-col space-y-4">
           <input
             className="text-xs p-3 rounded outline-none"
             type="email"
             id="email"
             name="email"
-            placeholder="your email"
+            placeholder="email"
+          />
+          <input
+            className="text-xs p-3 rounded outline-none"
+            type="text"
+            id="firstname"
+            name="firstname"
+            placeholder="firstname"
+          />
+          <input
+            className="text-xs p-3 rounded outline-none"
+            type="text"
+            id="lastname"
+            name="lastname"
+            placeholder="lastname"
           />
           <input
             className="text-xs p-3 rounded outline-none"
             type="password"
             id="password"
             name="password"
-            placeholder="your password"
+            placeholder="password"
           />
-          <ButtonForm value="Sign-In" type="submit" action="signIn"/>
-          <ButtonForm value="Register here" type="classic" action="newRegister"/>
+          <ButtonForm value="Register" type="submit" action="signUp"/>
+          <ButtonForm value="Sign-In" type="classic" action="goHome"/>
         </form>
       </div>
     </div>
